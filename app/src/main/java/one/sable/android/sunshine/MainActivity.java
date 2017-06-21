5d206fb1d8d2f148d30435fe7ba0721583aaed21
VNV <n.voronov@bk.ru>
2017-06-21 10:46:24 +0400

    added first xmls

--- app/src/main/java/one/sable/android/sunshine/MainActivity.java
+++ app/src/main/java/one/sable/android/sunshine/MainActivity.java
@@ -2,6 +2,10 @@ package one.sable.android.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.support.v4.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction().
+			add(R.id.container , new PlaceholderFragment()).
+			commit();
+		}
     }
+	
+	public static class PlaceholderFragment extends Fragment {
+		public PlaceholderFragment(){
+			
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater,
+			ViewGroup container, Bundle savedInstanceState)
+		{
+			View rootView = inflater.inflate(R.layout.fragment_main,container, false);
+			return rootView;
+		}
+		
+	}
 }
