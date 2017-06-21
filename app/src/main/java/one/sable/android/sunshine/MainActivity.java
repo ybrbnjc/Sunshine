de4a93f16afb043f860eda1abf4f83638ce9792a
Nikita Voronov <n.voronov@bk.ru>
2017-06-21 10:56:51 +0300

    formated

--- app/src/main/java/one/sable/android/sunshine/MainActivity.java
+++ app/src/main/java/one/sable/android/sunshine/MainActivity.java
@@ -13,25 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction().
-			add(R.id.container , new PlaceholderFragment()).
-			commit();
-		}
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().
+                    add(R.id.container, new PlaceholderFragment()).
+                    commit();
+        }
     }
-	
-	public static class PlaceholderFragment extends Fragment {
-		public PlaceholderFragment(){
-			
-		}
 
-		@Override
-		public View onCreateView(LayoutInflater inflater,
-			ViewGroup container, Bundle savedInstanceState)
-		{
-			View rootView = inflater.inflate(R.layout.fragment_main,container, false);
-			return rootView;
-		}
-		
-	}
+    public static class PlaceholderFragment extends Fragment {
+        public PlaceholderFragment() {
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater,
+                                 ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+    }
 }
