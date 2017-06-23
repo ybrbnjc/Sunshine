a367519a15365216466da85c6a44cf21c5a10817
VNV <n.voronov@bk.ru>
2017-06-24 00:26:12 +0400

    added listview with fake data

--- app/src/main/java/one/sable/android/sunshine/MainActivity.java
+++ app/src/main/java/one/sable/android/sunshine/MainActivity.java
@@ -6,6 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
 import android.support.v4.app.Fragment;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +26,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static class PlaceholderFragment extends Fragment {
-        public PlaceholderFragment() {
+        
+		private ArrayAdapter<String> mForecastAdapter;
+		
+		public PlaceholderFragment() {
 
         }
 
@@ -29,6 +37,26 @@ public class MainActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater,
                                  ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+			
+			String[] fakeDataArray = new String[] {
+			"Yesterday - Rainy - 17/8",
+			"Today - Cloudy - 16/8",
+			"Tomorrow - Rainy - 18/6",
+			"Saturday - Cloudy - 19/10",
+			"Sunday - Rainy - 20/13",
+			"Monday - Rainy - 22/10",
+			"Tuesday - Foggy - 30/25"
+			};
+			List<String> fakeData = new ArrayList<String>(Arrays.asList(fakeDataArray));
+			
+			mForecastAdapter = new ArrayAdapter<String> (
+				getActivity(),
+				R.layout.list_item_forecast,
+				R.id.list_item_forecast_text_view,
+				fakeData);
+				
+			ListView forecastListview = (ListView) rootView.findViewById(R.id.listview_forecast);
+			forecastListview.setAdapter(mForecastAdapter);
             return rootView;
         }
 
